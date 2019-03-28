211f80d3a50e7420a880fe5d39f2a3e35af016d5
Heeyeong Kim <gldud9833@gmail.com>
2019-03-28 18:10:41 -0400

    Edited endpoint

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "URI" : "mongodb://localhost/demo",
-    //"URI": "mongodb://heeyeong:a123456@ds058739.mlab.com:58739/comp308-w2019-meanportfolio",
+    //"URI" : "mongodb://localhost/demo",
+    "URI": "mongodb://heeyeong:a123456@ds058739.mlab.com:58739/comp308-w2019-meanportfolio",
     "secret": "SomeSecret"
 } 
